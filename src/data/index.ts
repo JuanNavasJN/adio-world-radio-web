6321cda2b48850ddd799572c5948c2c4fbe890e4
Juan Navas <iam@juannavas.dev>
2023-04-03 16:44:05 -0400

    Rename cover to coverUrl

--- src/data/index.ts
+++ src/data/index.ts
@@ -6,17 +6,17 @@ export const countriesStations = [
       {
         name: 'Radio Comercial',
         url: 'https://stream-icy.bauermedia.pt/comercial.aac',
-        cover: '/stations-logos/radio-comercial.png'
+        coverUrl: '/stations-logos/radio-comercial.png'
       },
       {
         name: 'Cidade FM',
         url: 'https://stream-icy.bauermedia.pt/cidade.mp3',
-        cover: '/stations-logos/cidade-fm.png'
+        coverUrl: '/stations-logos/cidade-fm.png'
       },
       {
         name: 'Antena 2 Jazz In',
         url: 'https://radiocast.rtp.pt/antena2jazzin80a.mp3',
-        cover: '/stations-logos/antena-jazz-in.png'
+        coverUrl: '/stations-logos/antena-jazz-in.png'
       }
     ]
   },
@@ -27,12 +27,12 @@ export const countriesStations = [
       {
         name: 'RMF FM',
         url: 'https://rs101-krk-cyfronet.rmfstream.pl/rmf_fm',
-        cover: '/stations-logos/rmf-fm.png'
+        coverUrl: '/stations-logos/rmf-fm.png'
       },
       {
         name: 'VOX FM',
         url: 'https://ic1.smcdn.pl/3990-1.mp3',
-        cover: '/stations-logos/vox-fm.png'
+        coverUrl: '/stations-logos/vox-fm.png'
       }
     ]
   },
@@ -43,12 +43,12 @@ export const countriesStations = [
       {
         name: 'NRJ Sverige',
         url: 'https://live-bauerse-fm.sharp-stream.com/nrj_instreamtest_se_mp3',
-        cover: '/stations-logos/nrj-sverige.png'
+        coverUrl: '/stations-logos/nrj-sverige.png'
       },
       {
         name: 'Sveriges Radio P3',
         url: 'https://http-live.sr.se/p3-mp3-192',
-        cover: '/stations-logos/sverige-radio-3.png'
+        coverUrl: '/stations-logos/sverige-radio-3.png'
       }
     ]
   },
@@ -59,12 +59,12 @@ export const countriesStations = [
       {
         name: '973FM: Blasts That Last',
         url: 'https://c16.radioboss.fm:18115/stream',
-        cover: '/stations-logos/973fm.jpg'
+        coverUrl: '/stations-logos/973fm.jpg'
       },
       {
         name: 'Symphony 924',
         url: 'https://22393.live.streamtheworld.com/SYMPHONY924_PREM.aac',
-        cover: '/stations-logos/symphony-924.jpg'
+        coverUrl: '/stations-logos/symphony-924.jpg'
       }
     ]
   },
@@ -75,17 +75,17 @@ export const countriesStations = [
       {
         name: 'Total Hits',
         url: 'https://totalhits128-thehitsnetwork.radioca.st/;',
-        cover: '/stations-logos/total-hits.webp'
+        coverUrl: '/stations-logos/total-hits.webp'
       },
       {
         name: 'BBC Radio 1',
         url: 'https://stream.live.vc.bbcmedia.co.uk/bbc_radio_one',
-        cover: '/stations-logos/bbc-1.webp'
+        coverUrl: '/stations-logos/bbc-1.webp'
       },
       {
         name: 'BBC Radio 2',
         url: 'https://stream.live.vc.bbcmedia.co.uk/bbc_radio_two',
-        cover: '/stations-logos/bbc-2.webp'
+        coverUrl: '/stations-logos/bbc-2.webp'
       }
     ]
   },
@@ -96,22 +96,22 @@ export const countriesStations = [
       {
         name: 'Antena 1 - Brasília',
         url: 'https://antenaone.crossradio.com.br/stream/1;?1649321773279',
-        cover: '/stations-logos/antena-1-brasilia.jpg'
+        coverUrl: '/stations-logos/antena-1-brasilia.jpg'
       },
       {
         name: 'Hits40Station',
         url: 'https://listen.openstream.co/3908/audio',
-        cover: '/stations-logos/hits-40.webp'
+        coverUrl: '/stations-logos/hits-40.webp'
       },
       {
         name: 'Rádio Globo Rio',
         url: 'https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_GLOBO_RJAAC.aac',
-        cover: '/stations-logos/radio-globo.png'
+        coverUrl: '/stations-logos/radio-globo.png'
       },
       {
         name: 'Rádio Bossa Nova Brazil',
         url: 'https://stream.streamgenial.stream/87eu2988rm0uv',
-        cover: '/stations-logos/bossa-nova.webp'
+        coverUrl: '/stations-logos/bossa-nova.webp'
       }
     ]
   },
@@ -122,22 +122,22 @@ export const countriesStations = [
       {
         name: 'Радио Zhuldyz FM',
         url: 'https://www.liveradio.es/http://91.201.214.229:8000/zhulduz',
-        cover: '/stations-logos/zhulduz.jpg'
+        coverUrl: '/stations-logos/zhulduz.jpg'
       },
       {
         name: 'Business FM',
         url: 'https://kvs01.businessfm.kz/businessfm',
-        cover: '/stations-logos/business-fm.jpg'
+        coverUrl: '/stations-logos/business-fm.jpg'
       },
       {
         name: 'Radio Classic - Astana KZ',
         url: 'https://radio-streams.kaztrk.kz/classic/classic/icecast.audio',
-        cover: '/stations-logos/radio-classic.jpg'
+        coverUrl: '/stations-logos/radio-classic.jpg'
       },
       {
         name: 'Love Radio',
         url: 'https://stream.gakku.tv:8443/love128.mp3',
-        cover: '/stations-logos/love-radio.jpg'
+        coverUrl: '/stations-logos/love-radio.jpg'
       }
     ]
   }
@@ -148,7 +148,7 @@ export const countriesStations = [
   //     {
   //       name: '',
   //       url: '',
-  //       cover: '/stations-logos/.png'
+  //       coverUrl: '/stations-logos/.png'
   //     },
   //   ]
   // },
