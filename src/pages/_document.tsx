d74cd801a75b1e4519b3bd57044974df5c87e1c5
Juan Navas <iam@juannavas.dev>
2023-05-24 15:50:32 -0400

    Add apple-touch-icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Adio World Radio - FM Online" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/adio-world-radio.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,7 @@ export default function Home() {
     <>
       <Head>
         <title>Adio World Radio - FM Online</title>
-        <meta name="description" content="Adio World Radio - FM Online" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <StationsProvider>
         <main className={styles.main}>
