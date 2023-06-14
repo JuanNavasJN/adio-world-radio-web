91785eddcf3293ddfb9de1f1085467a5055d4dab
Juan Navas <iam@juannavas.dev>
2023-06-14 16:21:23 -0400

    Add nunito font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import { fetcher } from '@/helpers';
 import type { AppProps } from 'next/app';
 import { SWRConfig } from 'swr';
+
 import '@/styles/globals.css';
+import '@fontsource/nunito';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
