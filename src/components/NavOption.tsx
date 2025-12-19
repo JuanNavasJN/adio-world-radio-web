21e54daebaaacd8e3df4fe208064e306967eea03
Juan Navas <iam@juannavas.dev>
2025-12-19 12:33:32 -0400

    add svg country flags

--- src/components/NavOption.tsx
+++ src/components/NavOption.tsx
@@ -1,5 +1,20 @@
 import React, { FC } from 'react';
 import styles from '@/styles/NavOption.module.css';
+import {
+  VE,
+  BR,
+  CZ,
+  FR,
+  DE,
+  PL,
+  PT,
+  RS,
+  ZA,
+  SE,
+  GB,
+  UA,
+  US
+} from 'country-flag-icons/react/3x2';
 
 interface NavOptionProps {
   label: string;
@@ -7,6 +22,22 @@ interface NavOptionProps {
   isActive?: boolean;
   onClick: (country: string) => void;
 }
+
+const flags = {
+  Bra: <BR />,
+  Ven: <VE />,
+  Cze: <CZ />,
+  Fra: <FR />,
+  Ger: <DE />,
+  Pol: <PL />,
+  Por: <PT />,
+  Ser: <RS />,
+  ZA: <ZA />,
+  Swe: <SE />,
+  UK: <GB />,
+  Ukr: <UA />,
+  US: <US />
+};
 
 const NavOption: FC<NavOptionProps> = ({
   label,
@@ -19,7 +50,7 @@ const NavOption: FC<NavOptionProps> = ({
       className={`${styles.navOption} ${isActive && styles.isActive}`}
       onClick={() => onClick(value)}
     >
-      {label}
+      {label} {flags[label as keyof typeof flags]}
     </button>
   );
 };
